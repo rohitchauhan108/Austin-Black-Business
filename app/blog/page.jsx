63c4cf5101a4e93207b31a7bd189378f7404d179
rohitchauhan09 <rohitchauhan9389@gmail.com>
2026-09-15 15:17:02 +0530

    checklist

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import BlogPage from '../../components/BlogPage.jsx';
 
 export default function Blog() {
-  return <BlogPage />;
+  return (
+    <Suspense fallback={null}>
+      <BlogPage />
+    </Suspense>
+  );
 }
